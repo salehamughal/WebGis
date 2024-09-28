b21c8286616ee0d4af59bed37d6462ece77a749a
salehamughal <salehanadeemm@gmail.com>
2024-09-28 02:23:54 -0700

    Add files via upload

---
+++ functions in javasacript.js
@@ -0,0 +1,14 @@
+function nice(name){
+    console.log("Hello "+ name)
+}
+nice("Adil")
+ 
+function sum(a,b){
+    console.log(a+b)
+}
+// Other way to creating function
+sum (1,2);
+let func=(x)=>{
+    console.log("hello " + x)
+}
+func("Limia")
