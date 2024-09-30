b32b8897c0d3434f31d312ffee14fddfe5fb78d1
salehamughal <salehanadeemm@gmail.com>
2024-09-30 09:26:58 -0700

    Add files via upload

---
+++ string in .js
@@ -0,0 +1,19 @@
+console.log("Welcome to string topic")
+let a ="Mughal";
+let b= "saleha";
+let c= "Limia";
+// we can access the string's number individually using indexes
+console.log(a[0])
+// we can create the lower or bottom case of the string using to lowerCase and to upperCase
+console.log(a.toLowerCase())
+console.log(a.toUpperCase())
+// we can count the length of the stirng
+console.log(a.length)
+// we can replace the string's number with any other number
+console.log(a.replace("M","a"))
+// We can replace string's "" to-> `` Following is the example
+console.log(b + " Is the best friend of the " + c);
+//  To->
+console.log (`${b} is the best friend of the ${c}`);
+
+
